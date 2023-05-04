4b97ce4c72b5ae8b9a2746be03c5d60546ba1f9b
Raquel Ramos Backes <71186654+Raquel32132@users.noreply.github.com>
2023-05-04 12:24:46 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { Home } from './pages/Home/index.tsx'
-import { Login } from './pages/Login/index.tsx';
+// import { Login } from './pages/Login/index.tsx';
 
 import './global.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Login />
+    <Home />
   </React.StrictMode>,
 )
